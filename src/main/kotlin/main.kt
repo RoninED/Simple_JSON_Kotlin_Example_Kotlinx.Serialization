64aa16862e20ecdb906fab2ce623afa966c09952
Eduard Gimadiev <nameeduard@yandex.ru>
2020-02-03 17:19:32 +0300

    looks like true

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,25 @@
 import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.*
 
+@Serializable
+data class Data(val a: Int, val b: String = "42")
 
 fun main() {
+    // Json also has .Default configuration which provides more reasonable settings,
+    // but is subject to change in future versions
+    val json = Json(JsonConfiguration.Stable)
+    // serializing objects
+    val jsonData = json.stringify(Data.serializer(), Data(42))
+    // serializing lists
+    val jsonList = json.stringify(Data.serializer().list, listOf(Data(42)))
+    println(jsonData) // {"a": 42, "b": "42"}
+    println(jsonList) // [{"a": 42, "b": "42"}]
+
+    // parsing data back
+    val obj = json.parse(Data.serializer(), """{"a":42}""") // b is optional since it has default value
+    println(obj) // Data(a=42, b="42")
 
 
-    @Serializable
-    data class Data(
-        val text:String = "text_test"
-    )
-
-    var json: Json = Data.serializer
+    
 }
-
 
